c841c108e2a812905d422dd6af56f68ed3849d5d
Da Eun <didekdms5502@gmail.com>
2025-12-17 23:52:54 +0900

    add test text

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-
 # 날짜 계산
 today = datetime.today()
 month_ago = today + relativedelta(months=-1)
@@ -13,17 +12,43 @@ day = today.day
 month_ago_2 = today + relativedelta(months=-2)
 month_2 = month_ago_2.strftime('%m')
 
+st.markdown(
+    """
+    <style>
+        .block-container {
+            padding-top: 2rem;
+            padding-bottom: 2rem;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # 주제
-st.title("🔍Search Keyword Trend Analysis")
+st.title("🔍검색 키워드 트렌드 분석 자동화")
 
 # 헤더
-st.markdown(f"## {year}년 {month}월 {day}일 기준 검색어 Summary")
+st.markdown(
+    f"""
+    <h5 style="margin-bottom: 0.5rem; color: gray;">
+        {year}년 {month}월 {day}일 기준 검색어 Summary
+    </h4>
+    """,
+    unsafe_allow_html=True
+)
+
 st.markdown('### 일별 검색어 집계', help='전일대비 증감')
 
-st.metric(label="총 검색량", value="30,083", delta="1.2 %")
+day, month, year = st.columns(3)
+day.metric(label="Daily", value="30,080", delta="3.8%",  border=True)
+month.metric(label="Daily", value="728,459", delta="1.5%",  border=True)
+year.metric(label="Daily", value="5,897,125", delta="1.8%",  border=True)
 
-col1, col2, col3 = st.columns(3)
-col1.metric("노출수", "30,083", "1.2 %")
-col2.metric("클릭수", "1,585", "-8%")
-col3.metric("CTR", "5.3%", "-4%")
+
+
+
+
+#col1, col2, col3 = st.columns(3)
+#col1.metric("노출수", "30,083", "1.2 %")
+#col2.metric("클릭수", "1,585", "-8%")
+#col3.metric("CTR", "5.3%", "-4%")
