9ecc4791ec71be5a081447f016ce37b9d68a474c
Da Eun <didekdms5502@gmail.com>
2025-12-17 23:33:56 +0900

    add test text

--- app.py
+++ app.py
@@ -5,19 +5,25 @@ from dateutil.relativedelta import relativedelta
 
 # 날짜 계산
 today = datetime.today()
-
 month_ago = today + relativedelta(months=-1)
 year = today.year
 month = month_ago.strftime('%m')
+day = today.day
 
 month_ago_2 = today + relativedelta(months=-2)
 month_2 = month_ago_2.strftime('%m')
 
 
 # 주제
-st.title("🔍검색 키워드 트렌드 분석 자동화")
+st.title("🔍Search Keyword Trend Analysis")
 
-# 헤더/KPI
-st.header("Summary")
-st.markdown(f"## {year}년 {month}월 기준 검색어 요약")
-st.markdown('### 월간 검색어 집계', help='전월대비 증감')
+# 헤더
+st.markdown(f"## {year}년 {month}월 {day}일 기준 검색어 Summary")
+st.markdown('### 일별 검색어 집계', help='전일대비 증감')
+
+st.metric(label="총 검색량", value="30,083", delta="1.2 %")
+
+col1, col2, col3 = st.columns(3)
+col1.metric("노출수", "30,083", "1.2 %")
+col2.metric("클릭수", "1,585", "-8%")
+col3.metric("CTR", "5.3%", "-4%")
