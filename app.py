98cacbb6a4db72d11ebd36d3925c086c75093f29
Da Eun <didekdms5502@gmail.com>
2025-12-17 23:03:32 +0900

    add test text

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
-st.write("test")
+today = datetime.today()
+month_ago = datetime(today.year, today.month, today.day) + relativedelta(months=-1)
+year = today.year
+month = "{}".format(month_ago.strftime('%m'))
+month_ago_2 = datetime(today.year, today.month, today.day) + relativedelta(months=-2)
+month_2 = "{}".format(month_ago_2.strftime('%m'))
 
-st.write("testttttttttttttttttttttttt")
+# 주제
+st.title("🔍검색 키워드 트렌드 분석 자동화")
 
-st.write("sss")
+# 헤더/KPI
+st.header("Summary")
+st.markdown(f"## {year}년 {month}월 기준 검색어 요약")
+st.markdown('### 월간 검색어 집계', help='전월대비 증감')
